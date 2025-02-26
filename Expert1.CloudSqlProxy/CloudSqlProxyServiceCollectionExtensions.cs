6c18fc1455a35be74250ac8ba98cc8686da5fd85
Lachlan Ennis <2433737+elachlan@users.noreply.github.com>
2025-02-27 07:54:22 +1000

    Add CloudSqlProxyServiceCollectionExtensions

---
+++ Expert1.CloudSqlProxy/CloudSqlProxyServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+namespace Expert1.CloudSqlProxy
+{
+    /// <summary>
+    /// Cloud Sql Proxy specific extension methods for <see cref="IServiceCollection" />.
+    /// </summary>
+    public static class CloudSqlProxyServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Registers a Cloud SQL Proxy instance as a singleton in the dependency injection container.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="authenticationMethod">authentication method</param>
+        /// <param name="instance">instance</param>
+        /// <param name="credentials">credential file or json</param>
+        /// <returns>The same service collection so that multiple calls can be chained.</returns>
+        public static IServiceCollection AddCloudSqlProxy(
+            this IServiceCollection services,
+            AuthenticationMethod authenticationMethod,
+            string instance,
+            string credentials) 
+            => services.AddSingleton(provider 
+                => ProxyInstance.StartProxy(authenticationMethod, instance, credentials));
+    }
+}
